1966317164c84f19f2a8d23ea1c2fee30c524929
shaikmohammadrafi77 <MOHAMMADRAFI6633@GMAIL.COM>
2025-09-13 22:11:30 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ python3 -m venv venv
 . venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-pkill -f "python3 app.py" || true
+python3 app.py || true
 nohup python3 app.py >app.log 4>&1 &
 echo "deployment succesfully!"
