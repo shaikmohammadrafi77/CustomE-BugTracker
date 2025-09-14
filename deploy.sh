0a2c54ef08b495cdead099fb62d6042dc520d5a0
shaikmohammadrafi77 <MOHAMMADRAFI6633@GMAIL.COM>
2025-09-13 21:15:53 -0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd ${APP_DIR}
+git pull origin main
+
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+pkill -f "python3 app.py" || true
+nohup python3 app.py >app.log 2>&1 &
+echo "deployment succesfully!"
