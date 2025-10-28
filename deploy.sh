12678d3cbe403a98b3d17210a5d831e1c785d920
shaikmohammadrafi77 <MOHAMMADRAFI6633@GMAIL.COM>
2025-10-28 11:10:49 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cd /home/ec2-user/CustomE-BugTracker || git clone -b main https://github.com/shaikmohammadrafi77/CustomE-BugTracker.git /home/ec2-user/CustomE-BugTracker
+cd /home/ec2-user/CustomE-BugTracker || git clone https://github.com/shaikmohammadrafi77/CustomE-BugTracker.git /home/ec2-user/CustomE-BugTracker
 git pull origin main
 
 python3 -m venv venv
