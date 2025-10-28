5a2464d38068599350bfec2f83e0ca3df1e60b75
shaikmohammadrafi77 <MOHAMMADRAFI6633@GMAIL.COM>
2025-10-28 09:53:03 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 set -e
+cd /home/ec2-user/CustomE-BugTracker || git clone -b main https://github.com/shaikmohammadrafi77/CustomE-BugTracker.git /home/ec2-user/CustomE-BugTracker
+git pull origin main
 
-APP_DIR="/home/ec2-user/app"
-REPO_URL="https://github.com/shaikmohammadrafi77/CustomE-BugTracker.git"
-
-# Create or update app directory
-cd $APP_DIR
-
-# If repo not cloned yet, clone it
-if [ ! -d ".git" ]; then
-    git clone $REPO_URL .
-else
-    git pull origin main
-fi
-
-# Stop any existing app.py process
-echo "Stopping old app process (if running)..."
-pkill -f "python3 app.py" || true
-
-# Setup virtual environment
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Start application
 nohup python3 app.py > app.log 2>&1 &
-
 echo "✅ Deployment successful!"
